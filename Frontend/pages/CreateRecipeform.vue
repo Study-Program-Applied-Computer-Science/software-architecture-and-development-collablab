48bf4e37f2d9bb9f22d3b75f0e2bec4fb6fb3438
Mohith Tummala <137559580+DevMohith@users.noreply.github.com>
2025-01-20 11:46:07 +0100

    Added mode pages in Frontend

---
+++ Frontend/pages/CreateRecipeform.vue
@@ -0,0 +1,11 @@
+<template>
+    <h1>Post Recipe Form</h1>
+  </template>
+  
+  <script>
+  
+  </script>
+  
+  <style>
+  
+  </style>
---
+++ Frontend/pages/UserProfile.vue
@@ -0,0 +1,11 @@
+<template>
+    <h1>UserAuth</h1>
+  </template>
+  
+  <script>
+  
+  </script>
+  
+  <style>
+  
+  </style>
