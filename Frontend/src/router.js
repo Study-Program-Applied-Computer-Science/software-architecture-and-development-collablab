04b3d8c92ade4c0d22a93bb8a8ccf9f03dae920c
neubiii <neubinsebastian01@gmail.com>
2025-01-21 11:30:54 +0100

    Fixed frontend folder structure and routes issue. Now pages getting routed properly.

--- Frontend/src/router.js
+++ Frontend/src/router.js
@@ -1,5 +1,4 @@
-import Vue from "vue";
-import Router from "vue-router";
+import { createRouter, createWebHistory } from "vue-router";
 
 import HomePage from "./pages/HomePage.vue";
 import SmartPantry from "./pages/SmartPantry.vue";
@@ -8,40 +7,43 @@ import RecipesPage from "./pages/RecipesPage.vue";
 import  UserProfile from "./pages/UserProfile.vue";
 import UserAuth from "./pages/UserAuth.vue";
 
-Vue.use(Router);
 
-export default new Router({
-  mode: "history",
-  routes: [
-    {
-      path: "/",
-      name: "home",
-      component: HomePage,
+const routes= [
+  {
+    path: "/",
+    name: "home",
+    component: HomePage,
+  },
+  {
+    path: "/smartpantry",
+    name: "smartpantry",
+    component: SmartPantry,
+  },
+  {
+    path: "/recipes",
+    name: "recipes",
+    component: RecipesPage,
+  },
+  {
+    path: "/profile",
+    name: "profile",
+    component: UserProfile,
+  },
+  {
+      path: "/login",
+      name: "login",
+      component: UserAuth,
     },
     {
-      path: "/smartpantry",
-      name: "smartpantry",
-      component: SmartPantry,
+      path: "/recipeform",
+      name: "recipeform",
+      component: CreateRecipeform,
     },
-    {
-      path: "/recipes",
-      name: "recipes",
-      component: RecipesPage,
-    },
-    {
-      path: "/profile",
-      name: "profile",
-      component: UserProfile,
-    },
-    {
-        path: "/login",
-        name: "login",
-        component: UserAuth,
-      },
-      {
-        path: "/recipeform",
-        name: "recipeform",
-        component: CreateRecipeform,
-      },
-  ],
+];
+
+const router = createRouter({
+  history: createWebHistory(),
+  routes,
 });
+
+export default router;
