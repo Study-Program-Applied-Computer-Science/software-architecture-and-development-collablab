6550c3d2dfa122d6aeb4a39a9735bc35c59ed4d4
Mohith Tummala <137559580+DevMohith@users.noreply.github.com>
2025-01-23 14:03:01 +0100

    Developed SmartPantry and implemented matching of recipe's data and smart pantry data and created specific endpoint for smart pantry

--- Frontend/vue.config.js
+++ Frontend/vue.config.js
@@ -1,12 +1,13 @@
-const { defineConfig } = require('@vue/cli-service')
-module.exports = defineConfig({
-  transpileDependencies: true
-})
+const { defineConfig } = require('@vue/cli-service');
+const webpack = require('webpack');
 
-module.exports = {
+module.exports = defineConfig({
+  transpileDependencies: true,
   configureWebpack: {
-    define: {
-      __VUE_PROD_HYDRATION_MISMATCH_DETAILS__: false,
-    },
+    plugins: [
+      new webpack.DefinePlugin({
+        __VUE_PROD_HYDRATION_MISMATCH_DETAILS__: JSON.stringify(false),
+      }),
+    ],
   },
-};
+});
